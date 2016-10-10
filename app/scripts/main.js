17a6eafce16e05708ba5d25a15d8f6e5c0630f60
Max Ehnert <max.ehnert@gmail.com>
2016-10-09 22:55:39 -0500

    Updated streams.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,7 +5,8 @@ const streamURLPostfix = '.m3u8';
 
 const clickChannel = () => {
   console.log(event.target.dataset.channel);
-  let newStream = `${streamUrlPrefix}${event.target.dataset.channel}${streamURLPostfix}`;
+  // let newStream = `${streamUrlPrefix}${event.target.dataset.channel}${streamURLPostfix}`;
+  let newStream = 'http://cf2.myomnibox.com/slc-rc-2/cnn/playlist.m3u8';
   console.log(video.src);
 
   hls.destroy();
@@ -32,9 +33,10 @@ hls.attachMedia(video);
 hls.on(Hls.Events.MEDIA_ATTACHED,function() {
     console.log("video and hls.js are now bound together !");
     const loadInitialChannel = document.querySelector('.marketing ul > li').dataset.channel;
-    hls.loadSource(`${streamUrlPrefix}${loadInitialChannel}${streamURLPostfix}`);
+    // hls.loadSource(`${streamUrlPrefix}${loadInitialChannel}${streamURLPostfix}`);
     // hls.loadSource("http://www.streambox.fr/playlists/x36xhzz/x36xhzz.m3u8");
-
+    // hls.loadSource('http://cf2.myomnibox.com/slc-rc-1/cbssports_temp/playlist.m3u8');
+    hls.loadSource('http://cf2.myomnibox.com/slc-rc-2/cnn/playlist.m3u8');
     hls.on(Hls.Events.MANIFEST_PARSED, function(event,data) {
      console.log("manifest loaded, found " + data.levels.length + " quality level");
    });
